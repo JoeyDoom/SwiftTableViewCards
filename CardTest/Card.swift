8741f716d41a8a24650d3fe1b1e8921ab78f352e
Joey Gordon <joey@theblackaxe.com>
2016-03-28 17:59:54 -0400

    refined the sections a bit more. also added support for header cells

--- CardTest/Card.swift
+++ CardTest/Card.swift
@@ -1,33 +1,25 @@
 //
-//  CardDataSource.swift
+//  Card.swift
 //  CardTest
 //
 //  Created by Joey Gordon on 3/22/16.
 //  Copyright © 2016 The Black Axe. All rights reserved.
 //
+//  This file sets up the Card data type.
 
 import Foundation
-
-//class Section {
-//    
-//    var cards: [Card]
-//    
-//    init(cards: [Card]) {
-//        self.cards = cards
-//    }
-//    
-//}
 
 class Card {
     
     // set up the variables
-    var title: String!
+    var title: String
     var content: String
+    var style: String?
     
-    
-    init(title: String, content: String) {
+    init(title: String, content: String, style: String? = "normal") {
         self.title = title
         self.content = content
+        self.style = style
     }
     
 }
--- CardTest/CardDataSource.swift
+++ CardTest/CardDataSource.swift
@@ -11,8 +11,16 @@ import Foundation
 class CardDataSource {
     var cards:[Card]
     var cards2:[Card]
+    var cards3:[Card]
+    var cardshead:[Card]
     
     init() {
+        // header section
+        cardshead = []
+        let cardhead = Card(title: "This Big Title", content: "who cares", style: "header")
+        cardshead.append(cardhead)
+        
+        // section 1
         cards = []
         let card1 = Card(title: "Naptown", content: "This plane is wiggling a lot. I hope that I don't spew everywhere.")
         cards.append(card1)
@@ -20,13 +28,28 @@ class CardDataSource {
         let card2 = Card(title: "Worldwide What", content: "It was bus #55")
         cards.append(card2)
         
+        // section 2
         cards2 = []
         let card3 = Card(title: "Chad Sexton", content: "Has Very Cool Drums")
         cards2.append(card3)
+        
+        let card6 = Card(title: "I needed another", content: "Whatever")
+        cards2.append(card6)
+        
+        // section 3
+        cards3 = []
+        let card4 = Card(title: "Leopard Spots", content: "Make for a very comfortable chair")
+        cards3.append(card4)
+        
+        let card5 = Card(title: "Alice Copper", content: "Also has a sword")
+        cards3.append(card5)
+        
+        let card7 = Card(title: "Wiggles", content: "Who Cares")
+        cards3.append(card7)
     }
     
     func getCards() -> [[Card]] {
-        return [cards, cards2]
+        return [cardshead, cards, cards2, cards3]
     }
     
 }
